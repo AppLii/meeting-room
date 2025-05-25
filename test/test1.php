0b7f1afc45e3999ca48d0e0268b71dccc34ed1d2
MatsumotoMahiro <2006mahiro0224@gmailcom>
2025-05-25 06:21:35 +0000

    fix: オートローダがちゃんと機能するようにした。

--- test/test1.php
+++ test/test1.php
@@ -1,10 +1,11 @@
 <?php
-use Database;
+require_once __DIR__ . '/../autoload.php';
+require_once __DIR__ . '/../app/util/utilFunctions.php';
 
 $db = Database::getInstance();
 
-$tables = $db->getAllTables;
+$tables = $db->getAllTables();
 
 foreach ($tables as $table) {
-	echoDataAsTable($table->getAllRecords);
+	echoDataAsTable($table->getAllRecords());
 }
